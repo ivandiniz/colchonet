dbd041cb10f7626d57c3849564c7d20eb30993b6
Ivan Diniz <ivandiniz@gmail.com>
2015-06-30 09:55:53 -0300

    CRUD inicial concluido

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, minimum: 30, allow_blank: false
-  validates_uniqueness_of :email
+  validates_format_of :email, with: EMAIL_REGEXXP
 
-  validate :email_format
+  has_secure_password
 
-  private
-  def email_format
-    errors.add(:email, :invalid) unless email.match(EMAIL_REGEXXP)
-  end
 end
